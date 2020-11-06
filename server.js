d3c7d05d7abdd4b8468825feb6cc75692a7e0d38
BestVersion7 <earthsmoonandspoon@yahoo.com>
2020-11-06 12:44:06 -0500

    import path to server

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
 const jwt = require("jsonwebtoken");
 // const pool = require("./database");
+const path = require('path')
 
 app.use(cors());
 app.use(express.json());
