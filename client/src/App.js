4c81f7c272c7311256697f4a5c8d892a941dacda
BestVersion7 <earthsmoonandspoon@yahoo.com>
2020-11-18 10:02:16 -0500

    react router update v5

---
+++ client/src/App.js
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from "react";
+import { Header } from "./components/header";
+import { Footer } from "./components/footer";
+import { Route, Switch } from "react-router-dom";
+import { Home } from "./routes/home";
+import { SignIn } from "./routes/signin";
+import { ErrorPage } from "./routes/errorPage";
+import { Donation } from "./routes/donation";
+import { Article } from "./routes/article";
+import { SignUp } from "./routes/signup";
+import { Master } from "./routes/master";
+import { MasterView } from "./routes/masterView";
+import ProtectedRoute from "./utilities/protectedRoute";
+import { AuthContext } from "./utilities/authContext";
+import { DonationSuccess } from "./routes/donationSuccess";
+import axios from "axios";
+
+const App = () => {
+    const [signedIn, setSignedIn] = useState(false);
+
+    // this is to change the navigation to remove signin to signout on refresh
+    const validateToken = async () => {
+        try {
+            await axios.get("/api/account");
+            setSignedIn(true);
+        } catch (err) {
+            console.clear();
+        }
+    };
+
+    useEffect(() => {
+        validateToken();
+    }, [signedIn]);
+
+    return (
+        <div>
+            <AuthContext.Provider value={{ signedIn, setSignedIn }}>
+                <Header />
+                <div className="container-app">
+                    <Switch>
+                        <Route path="/" exact>
+                            <Home />
+                        </Route>
+                        <Route path="/signin">
+                            <SignIn />
+                        </Route>
+                        <Route path="/signup">
+                            <SignUp />
+                        </Route>
+                        <Route path="/article/:articleId/:articleTitle">
+                            <Article />
+                        </Route>
+                        <Route
+                            path="/donate/success/:id"
+                            component={DonationSuccess}
+                        />
+                        <Route path="/donate">
+                            <Donation />
+                        </Route>
+                        <Route path="/master">
+                            <Master />
+                        </Route>
+                        <ProtectedRoute path="/masterview">
+                            <MasterView />
+                        </ProtectedRoute>
+                        <Route path="/">
+                            <ErrorPage />
+                        </Route>
+                    </Switch>
+                </div>
+                <Footer />
+            </AuthContext.Provider>
+        </div>
+    );
+};
+
+export default App;
